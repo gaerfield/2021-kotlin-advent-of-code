727484701eaa324a9468ce2105938b5936c51cb6
gaerfield <gaerfield@users.noreply.github.com>
2021-12-08 19:58:25 +0100

    restructure code

---
+++ src/main/kotlin/de/kramhal/aoc/twentyone/day0/Day0.kt
@@ -0,0 +1,9 @@
+package de.kramhal.aoc.twentyone.day0
+
+fun part1(input: List<String>): Int { return 0 }
+fun part2(input: List<String>): Int { return 0 }
+
+fun main() {
+    println(part1(emptyList()))
+    println(part2(emptyList()))
+}
---
+++ src/test/kotlin/de/kramhal/aoc/twentyone/day0/Day0Test.kt
@@ -0,0 +1,13 @@
+package de.kramhal.aoc.twentyone.day0
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class Day0Test {
+
+    @Test
+    fun part1() { assertEquals(0, part1(emptyList())) }
+
+    @Test
+    fun part2() { assertEquals(0, part2(emptyList())) }
+}
